439caedecd373d36865b41740387b74adc18cab7
ZHANG Heng <caszhmail@163.com>
2017-07-28 16:54:06 +0800

    gti commit

--- python/Assignment3/RNN_Captioning.py
+++ python/Assignment3/RNN_Captioning.py
@@ -1,6 +1,10 @@
-'''
+"""
 Created on 28 July, 2017
 Reference: CS231n
 Platform: win10 64-bits
 python: 3.6
-'''
+Object: 
+ (1) train a vanilla recurrent neural networks
+ (2) use them it to train a model that can generate novel captions for images
+"""
+from CS231n.gradient_check import eval_numerical_gradient, eval_numerical_gradient_array
